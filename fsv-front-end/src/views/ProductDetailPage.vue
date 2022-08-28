7642a793a4063d05e96b743f082768342f2ff556
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2022-08-28 13:54:20 -0700

    add data, styles, and images

--- fsv-front-end/src/views/ProductDetailPage.vue
+++ fsv-front-end/src/views/ProductDetailPage.vue
@@ -9,7 +9,33 @@
     </script>
     
     <style scoped>
-    h1 {
-        color: blue
-    }
-    </style>
+        #page-wrap {
+          margin-top: 16px;
+          padding: 16px;
+          max-width: 600px;
+        }
+      
+        #img-wrap {
+          text-align: center;
+        }
+      
+        img {
+          width: 400px;
+        }
+      
+        #product-details {
+          padding: 16px;
+          position: relative;
+        }
+      
+        #add-to-cart {
+          width: 100%;
+        }
+      
+        #price {
+          position: absolute;
+          top: 24px;
+          right: 16px;
+        }
+      </style>
+      

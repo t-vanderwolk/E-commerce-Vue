d38046158bfe2246f56ce171f328a92d3a733dac
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2022-08-28 15:01:33 -0700

    Create a product page

--- fsv-front-end/src/views/ProductsPage.vue
+++ fsv-front-end/src/views/ProductsPage.vue
@@ -1,10 +1,32 @@
 <template>
-<h1>This is the Products Page</h1>
+ <div id="page-wrap">
+<div class ="grid-wrap">
+    <div
+    v-for="product in products"
+    class="product-item"
+    v-bind:key="product.id"
+    >
+    <img v-bind:src="product.imageUrl" />
+        <h3 class="product-name">{{product.name}}</h3>
+        <p class="product-price">{{product.price}}</p>
+        <router-link v-bind:to="'/products/' + product.id">
+        
+        <button>Veiw Details</button>
+    </router-link>
+    </div>
+    </div>
+    </div>
 </template>
 
 <script>
+import { products } from '@/fake-data';
 export default {
-name: 'ProductsPage'
+name: 'ProductsPage',
+data () {
+return {
+    products,
+}
+}
 }
 </script>
 

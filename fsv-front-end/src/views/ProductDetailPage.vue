c0b05222bea3730373fd5e2609408e39e9e30b1e
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2022-08-28 15:29:18 -0700

    create a product detail page

--- fsv-front-end/src/views/ProductDetailPage.vue
+++ fsv-front-end/src/views/ProductDetailPage.vue
@@ -1,11 +1,31 @@
 <template>
-    <h1>This is the Product Detail Page</h1>
+
+   
+    <div id ="page-wrap">
+        <div id="img-wrap">
+        <img v-bind:src="product.imageUrl" />
+        </div>
+        <div id="product-details">
+            <h1>{{product.name}}</h1>
+            <h3 id="price">${{ product.price }}</h3>
+            <p>Average rating: {{ product.averageRating}}</p>
+            <button id="add-to-cart">Add to Cart</button>
+        <h4>Description</h4>
+        <p>{{ product.description }}</p>
+        </div>
+        </div>
     </template>
-    
     <script>
+        import { products } from '../fake-data';
     export default {
-    name: 'ProductDetailPage'
+    name: 'ProductDetailPage',
+    data() {
+
+    return {
+        product: products.find((p) => p.id === this.$route.params.id),
     }
+    }
+}
     </script>
     
     <style scoped>

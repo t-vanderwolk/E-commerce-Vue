f5c008f071ac3f8ca7b0d203d50124af06cb949d
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2022-08-29 09:58:35 -0700

    challange and solution: Modular components

--- fsv-front-end/src/views/CartPage.vue
+++ fsv-front-end/src/views/CartPage.vue
@@ -1,41 +1,32 @@
 <template>
     <div id="page-wrap">
     <h1>Shopping Cart</h1>
-    <div
-    v-for="product in cartItems"
-    :key="product.id"
-    class="product-container">
-    <img class="product-image" :src="product.imageUrl" />
-    <div class="details-wrap">
-        <h3>{{ product.name }}</h3>
-        <p>${{ product.price }}</p>
-    </div>
-    <button class="remove-button">Remove From Cart</button>
-    </div>
-    <h3 id="total-price">Total: ${{ totalPrice }}</h3>
+   <ProductsList :products="cartItems" />
+   <h3 id="total-price">Total: ${{ totalPrice }}</h3>
+    
+
     <button id="checkout-button"> Proceed To Checkout</button>
     </div>
     </template>
     
     <script>
     import { cartItems } from '../fake-data';
+    import ProductsList from '../components/ProductsList.vue';
 
     export default {
-    name: 'CartPage',
+    name: "CartPage",
     data() {
         return {
             cartItems,
-        }
+        };
     },
     computed: {
         totalPrice() {
-         return this.cartItems.reduce(
-            (sum, item) => sum + Number(item.price),
-            0,
-         );   
+            return this.cartItems.reduce((sum, item) => sum + Number(item.price), 0);
         }
-    }
-    }
+    },
+    components: { ProductsList }
+}
     </script>
     
     <style scoped>

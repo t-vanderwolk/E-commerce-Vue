16edd394eee0a025106342ae7a21a8b664ed76a5
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2022-08-29 12:00:16 -0700

    test

--- fsv-back-end/src/server.js
+++ fsv-back-end/src/server.js
@@ -6,7 +6,7 @@ app.get('/hello',(req, res) => {
     res.send('hello!');
 });
 app.post('/hello',(req, res) => {
-res.send(`Hello ${req.body.name}`)
+res.send(`Hello ${req.body.name}`);
 });
 app.listen(8000, () => {
     console.log('Server is Listening on Port 8000')

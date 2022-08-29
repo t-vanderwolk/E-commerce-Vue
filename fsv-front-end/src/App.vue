2490f17467e9c123cc081075ece27d9af1ece5b0
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2022-08-28 18:48:09 -0700

    Create a NavBar

--- fsv-front-end/src/App.vue
+++ fsv-front-end/src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+<NavBar/>
     <router-view/>
   </div>
 </template>
+
+<script>
+  import NavBar from './components/NavBar.vue';
+  export default {
+name: 'App',
+components: {
+  NavBar,
+}
+  }
+</script>
 
 <style>
   * {

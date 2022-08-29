0724dabea84fa450981fef1feb0d18b02da29f29
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2022-08-29 11:58:37 -0700

    Test servers with postman

--- fsv-back-end/src/server.js
+++ fsv-back-end/src/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
-
+import bodyParser from 'body-parser';
 const app= express();
-
+app.use(bodyParser.json());
 app.get('/hello',(req, res) => {
     res.send('hello!');
 });
-
+app.post('/hello',(req, res) => {
+res.send(`Hello ${req.body.name}`)
+});
 app.listen(8000, () => {
     console.log('Server is Listening on Port 8000')
 })

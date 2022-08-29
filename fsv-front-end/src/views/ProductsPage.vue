6ca2e69caf60046d4065ac53b87cc4f020c88f14
Taylor Vanderwolk <taylorvanderwolk@Taylors-MacBook-Pro.local>
2022-08-29 09:37:52 -0700

    Use modular compnents

--- fsv-front-end/src/views/ProductsPage.vue
+++ fsv-front-end/src/views/ProductsPage.vue
@@ -1,71 +1,23 @@
 <template>
- <div id="page-wrap">
-<div class ="grid-wrap">
-    <div
-    v-for="product in products"
-    class="product-item"
-    v-bind:key="product.id"
-    >
-    <img v-bind:src="product.imageUrl" />
-        <h3 class="product-name">{{product.name}}</h3>
-        <p class="product-price">{{product.price}}</p>
-        <router-link v-bind:to="'/products/' + product.id">
-        
-        <button>Veiw Details</button>
-    </router-link>
+    <div id="page-wrap">
+      <ProductsGrid :products="products" />
     </div>
-    </div>
-    </div>
-</template>
-
-<script>
-import { products } from '@/fake-data';
-export default {
-name: 'ProductsPage',
-data () {
-return {
-    products,
-}
-}
-}
-</script>
-
-<style scoped>
-    .grid-wrap {
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: space-between;
-      margin-top: 16px;
-    }
+  </template>
   
-    .product-item {
-      align-items: center;
-      border-radius: 8px;
-      box-shadow: 0px 2px 5px #888;
-      display: flex;
-      flex-direction: column;
-      margin-bottom: 2%;
-      padding: 20px;
-      position: relative;
-      width: 32%;
-    }
+  <script>
+  import { products } from '../fake-data';
+  import ProductsGrid from '../components/ProductsGrid.vue';
   
-    .product-name {
-      margin-bottom: 0;
-    }
-  
-    img {
-      height: 200px;
-      width: 200px;
-    }
-  
-    a {
-      width: 100%;
-    }
-  
-    button {
-      width: 100%;
-    }
-  </style>
-  
+  export default {
+      name: 'ProductsPage',
+      components: {
+        ProductsGrid,
+      },
+      data() {
+        return {
+          products,
+        };
+      }
+  };
+  </script>
   
